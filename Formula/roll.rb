5bf2a5ecdab15b6b4a1a0192beeffeb503e3e24f
Rick <rickb1998@hotmail.com>
2022-07-10 15:23:20 +0200

    change version

--- Formula/roll.rb
+++ Formula/roll.rb
@@ -1,11 +1,11 @@
 
-class Deck < Formula
+class Roll < Formula
   desc "RollDev is a CLI utility for working with docker-compose environments"
   homepage "https://getroll.dev"
   version "0.1.0-beta1"
-  url "https://github.com/dockergiant/deck/archive/0.1.0-beta1.tar.gz"
-  sha256 "d2a172e8ca880db2b94e4931bf31ac8fe98e38392bf43cbdb156b76626b43448"
-  head "https://github.com/dockergiant/roll.git", :branch => "develop"
+  url "https://github.com/dockergiant/rolldev/archive/0.1.0-beta1.tar.gz"
+  sha256 "390d0f3202c7ce22e2dc43ce5550285184fef45996b287f3c4436ae3dbe187ee"
+  head "https://github.com/dockergiant/rolldev.git", :branch => "develop"
 
   def install
     prefix.install Dir["*"]
@@ -13,19 +13,19 @@ class Deck < Formula
 
   def caveats
     <<~EOS
-      Deck manages a set of global services on the docker host machine. You
+      RollDev manages a set of global services on the docker host machine. You
       will need to have Docker installed and docker-compose available in your
-      local $PATH configuration prior to starting Deck.
+      local $PATH configuration prior to starting RollDev.
 
-      To start deck simply run:
+      To start roll simply run:
 
-        deck svc up
+        roll svc up
 
-      This command will automatically run "deck install" to setup a trusted
+      This command will automatically run "roll install" to setup a trusted
       local root certificate and sign an SSL certificate for use by services
-      managed by deck via the "deck sign-certificate deck.test" command.
+      managed by roll via the "roll sign-certificate roll.test" command.
 
-      To print a complete list of available commands simply run "deck" without
+      To print a complete list of available commands simply run "roll" without
       any arguments.
 
       Documentation is available at: https://docs.getroll.dev
