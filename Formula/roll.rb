56f79766061434d4af7b4b9d501e5f2ac43b01a8
Rick Bouma <r.bouma@epartment.nl>
2022-10-24 13:13:50 +0200

    add new version

--- Formula/roll.rb
+++ Formula/roll.rb
@@ -2,9 +2,9 @@
 class Roll < Formula
   desc "RollDev is a CLI utility for working with docker-compose environments"
   homepage "https://getroll.dev"
-  version "0.1.0-beta5"
-  url "https://github.com/dockergiant/rolldev/archive/0.1.0-beta5.tar.gz"
-  sha256 "e2b843b0e18f5aeb50203d03765cafa0c4aaefc2213ac38bef085841086acf2a"
+  version "0.1.0-beta6"
+  url "https://github.com/dockergiant/rolldev/archive/0.1.0-beta6.tar.gz"
+  sha256 "d93f4098b6723a59f2f4cada4134e76d71ea1f400137611ccccd1948485f36bc"
   head "https://github.com/dockergiant/rolldev.git", :branch => "develop"
 
   def install
@@ -28,7 +28,7 @@ class Roll < Formula
       To print a complete list of available commands simply run "roll" without
       any arguments.
 
-      Documentation is available at: https://docs.getroll.dev
+      Documentation is available at: Not Yet Implemented
     EOS
   end
 end
