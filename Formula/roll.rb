f6678d4dc414f04ea301eb8a9c8b44a9f9ac867b
Rick Bouma <r.bouma@epartment.nl>
2023-01-19 11:10:12 +0100

    bump version

--- Formula/roll.rb
+++ Formula/roll.rb
@@ -2,7 +2,7 @@
 class Roll < Formula
   desc "RollDev is a CLI utility for working with docker-compose environments"
   homepage "https://getroll.dev"
-  version "0.1.0-beta7"
+  version "0.1.0-beta8"
   url "https://github.com/dockergiant/rolldev/archive/0.1.0-beta8.tar.gz"
   sha256 "7f096220e9c3b3b7a0de6791ad7eb6a8b0839e248c44b285ca16aecdc7385999"
   head "https://github.com/dockergiant/rolldev.git", :branch => "develop"
@@ -23,7 +23,7 @@ class Roll < Formula
 
       This command will automatically run "roll install" to setup a trusted
       local root certificate and sign an SSL certificate for use by services
-      managed by roll via the "roll sign-certificate roll.test" command.
+      managed by roll via the "roll rolsign-certificate roll.test" command.
 
       To print a complete list of available commands simply run "roll" without
       any arguments.
